d663a899a111515d85f5bdcbbd4cf57e06041727
hogucc <uku.h1r8@gmail.com>
2021-10-24 22:11:58 +0900

    本の一覧ページにPDF出力リンクを追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,14 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
+
+    respond_to do |format|
+      format.html
+      format.pdf do
+        render pdf: "file_name",
+        layout: 'pdf_sample.html.slim',
+        template: 'books/index.html.slim'
+      end
+    end
   end
 end
